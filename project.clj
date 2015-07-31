5cc725eba49925e83418f0e3cfea5aee0fb3467e
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-31 15:47:53 +0200

    project: bump dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject clostack "0.1.3"
+(defproject clostack "0.1.4"
   :description "clojure cloudstack client"
   :url "https://github.com/pyr/clostack"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :plugins [[codox "0.6.4"]]
-  :dependencies [[org.clojure/clojure    "1.5.1"]
+  :dependencies [[org.clojure/clojure    "1.7.0"]
                  [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.json  "0.2.2"]
-                 [http.async.client      "0.5.2"]])
+                 [org.clojure/data.json  "0.2.6"]
+                 [http.async.client      "0.6.0"]])

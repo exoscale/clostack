df07df0807c83074b109c3ce6fa2208d0d4ccb0d
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-14 22:17:28 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject spootnik/clostack "0.2.4"
+(defproject spootnik/clostack "0.2.5"
   :description "clojure cloudstack client"
   :url "https://github.com/pyr/clostack"
   :license {:name "MIT License"}
   :plugins [[codox "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire            "5.5.0"]
-                 [spootnik/net        "0.2.6"]])
+                 [spootnik/net        "0.2.7"]])

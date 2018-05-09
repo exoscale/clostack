f7525408fd28dc37ac0f0c038367b1c914b4b597
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-05-09 08:39:13 +0200

    client: simplify paginate
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- src/clostack/client.clj
+++ src/clostack/client.clj
@@ -72,15 +72,12 @@
   ([client op args page width]
    (when (or (nil? width) (pos? width))
      (let [resp     (request client op (assoc args :page page :pagesize 500))
-           success? (= 2 (quot (:status resp) 100))]
-       (when-not success?
-         (throw (ex-info "could not perform paging request" {:resp resp})))
-       (let [desc     (->> resp :body (map val) (filter map?) first)
-             width    (or width (:count desc) 0)
-             elems    (->> desc (map val) (filter vector?) first)
-             pending  (- width (count elems))]
-         (when (seq elems)
-           (lazy-cat elems (paging-request client op args (inc page) pending))))))))
+           desc     (->> resp :body (map val) (filter map?) first)
+           width    (or width (:count desc) 0)
+           elems    (->> desc (map val) (filter vector?) first)
+           pending  (- width (count elems))]
+       (when (seq elems)
+         (lazy-cat elems (paging-request client op args (inc page) pending)))))))
 
 (defn polling-request
   "Perform a polling request, in a blocking fashion. Fetches are done every second."

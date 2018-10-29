6229de8d11bd8a80098d9eb7ea990b35ee1b5c79
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-10-29 07:39:37 +0100

    signature: pre condition to avoid npe
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/clostack "0.2.15"
+(defproject exoscale/clostack "0.2.16-SNAPSHOT"
   :description "clojure cloudstack client"
   :url "https://github.com/exoscale/clostack"
   :license {:name "MIT License"}
--- test/clostack/payload_test.clj
+++ test/clostack/payload_test.clj
@@ -6,10 +6,10 @@
 (def API_SECRET "secret")
 
 (deftest sign-case-sensitivity
-  (testing "sha1 signature case sensitivity")
-  (is (not= (sign {"a" "a" "b" "B"} API_SECRET)
-            (sign {"a" "a" "B" "B"} API_SECRET)))
-  (is (not= (sign {:a "a" :b "B"} API_SECRET)
-            (sign {:a "a" :B "B"} API_SECRET)))
-  (is (not= (sign {:a "a" :b [{:c 1 :d true}]} API_SECRET)
-            (sign {:a "a" :b [{:c 1 :D true}]} API_SECRET))))
+  (testing "sha1 signature case sensitivity"
+    (is (not= (sign {"a" "a" "b" "B"} API_SECRET)
+              (sign {"a" "a" "B" "B"} API_SECRET)))
+    (is (not= (sign {:a "a" :b "B"} API_SECRET)
+              (sign {:a "a" :B "B"} API_SECRET)))
+    (is (not= (sign {:a "a" :b [{:c 1 :d true}]} API_SECRET)
+              (sign {:a "a" :b [{:c 1 :D true}]} API_SECRET)))))

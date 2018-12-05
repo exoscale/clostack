f4bca23bc07db408e99c19b790eaa3f00d29e1cb
Ivan Grishaev <ivan@grishaev.me>
2018-12-05 13:19:18 +0300

    Add support for bytes as a signature's input (#20)
    * signature now supports byte array for the input

--- src/clostack/signature.clj
+++ src/clostack/signature.clj
@@ -1,17 +1,21 @@
 (ns clostack.signature
   "HMAC-SHA1 signing functions"
+  (:require [byte-streams :as bs])
   (:import javax.crypto.spec.SecretKeySpec
            javax.crypto.Mac
            java.util.Base64))
 
+
 (defn sha1-signature
   "Given a secret, compute the base64 encoded representation of a
    payload's Hmac-Sha1"
-  [^String secret ^String input]
+  [^String secret input]
   {:pre [(seq secret)]}
-  (let [key  (SecretKeySpec. (.getBytes secret) "HmacSHA1")
-        mac  (doto (Mac/getInstance "HmacSHA1") (.init key))]
+
+  (let [key (SecretKeySpec. (.getBytes secret) "HmacSHA1")
+        mac (doto (Mac/getInstance "HmacSHA1") (.init key))]
+
     (some->> input
-             .getBytes
+             bs/to-byte-array
              (.doFinal mac)
              (.encodeToString (Base64/getEncoder)))))
--- test/clostack/signature_test.clj
+++ test/clostack/signature_test.clj
@@ -8,3 +8,18 @@
                  (sha1-signature nil "input"))))
   (testing "sha1 signature without an input"
     (is (nil? (sha1-signature "secret" nil)))))
+
+
+
+(deftest sha1-signature-input
+  (testing "string and byte inputs give the same sig"
+    (let [secret "secret"
+          input "input"
+          sig0 "MEQPNt3CgJu9TIsfN6boDXWIwwM="
+          sig1 (sha1-signature secret input)
+          sig2 (sha1-signature secret (.getBytes input))]
+      (is (= sig0 sig1 sig2))))
+
+  (testing "other input type fails"
+    (is (thrown? Exception
+                 (sha1-signature "secret" 42)))))

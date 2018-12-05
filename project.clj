b62fb738a2760cdb12fa566fe50290536e3d19b9
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-12-05 11:21:52 +0100

    release 0.2.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/clostack "0.2.17-SNAPSHOT"
+(defproject exoscale/clostack "0.2.17"
   :description "clojure cloudstack client"
   :url "https://github.com/exoscale/clostack"
   :license {:name "MIT License"}

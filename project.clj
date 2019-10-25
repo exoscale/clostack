477f043cff4afe539402dea5e269418509cb702e
Bertrand Cachet <bertrand.cachet@exoscale.com>
2019-10-25 11:31:03 +0200

    Version 0.2.19

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
               [k version :exclusions exclusions])
       paths (:paths cfg)]
 
-  (defproject exoscale/clostack "0.2.19-SNAPSHOT"
+  (defproject exoscale/clostack "0.2.19"
     :description "clojure cloudstack client"
     :url "https://github.com/exoscale/clostack"
     :license {:name "MIT License"}

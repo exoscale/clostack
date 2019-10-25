28fd0390ef0eaeec2c61b6ea3b68a62bbd6dd836
Bertrand Cachet <bertrand.cachet@exoscale.com>
2019-10-25 10:45:39 +0200

    REF(build): Use deps/paths from deps.clj into project.clj

--- project.clj
+++ project.clj
@@ -1,14 +1,17 @@
-(defproject exoscale/clostack "0.2.18-SNAPSHOT"
-  :description "clojure cloudstack client"
-  :url "https://github.com/exoscale/clostack"
-  :license {:name "MIT License"}
-  :plugins [[lein-codox "0.10.5"]
-            [lein-ancient "0.6.15"]]
-  :global-vars {*warn-on-reflection* true}
-  :codox {:source-uri  "https://github.com/exoscale/clostack/blob/{version}/{filepath}#L{line}"
-          :output-path "docs"
-          :metadata    {:doc/format :markdown}}
-  :dependencies [[org.clojure/clojure    "1.10.0-beta6"]
-                 [cheshire               "5.8.1"]
-                 [clj-time               "0.15.1"]
-                 [aleph                  "0.4.6"]])
+(let [cfg   (clojure.edn/read-string (slurp "deps.edn"))
+      deps  (for [[k {:keys [mvn/version exclusions]}] (:deps cfg)]
+              [k version :exclusions exclusions])
+      paths (:paths cfg)]
+
+  (defproject exoscale/clostack "0.2.18-SNAPSHOT"
+    :description "clojure cloudstack client"
+    :url "https://github.com/exoscale/clostack"
+    :license {:name "MIT License"}
+    :plugins [[lein-codox "0.10.7"]
+              [lein-ancient "0.6.15"]]
+    :global-vars {*warn-on-reflection* true}
+    :codox {:source-uri  "https://github.com/exoscale/clostack/blob/{version}/{filepath}#L{line}"
+            :output-path "docs"
+            :metadata    {:doc/format :markdown}}
+    :dependencies ~deps
+    :source-paths ~paths))

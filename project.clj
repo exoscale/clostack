9b5e9f87d8603bc5b602ca25071cfdc66ec39905
Jeremy Van de Wyngaert <jeremy.vandewyngaert@exoscale.com>
2024-04-12 10:58:21 +0200

    Version 0.2.24-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
               [k version :exclusions exclusions])
       paths (:paths cfg)]
 
-  (defproject exoscale/clostack "0.2.23"
+  (defproject exoscale/clostack "0.2.24-SNAPSHOT"
     :description "clojure cloudstack client"
     :url "https://github.com/exoscale/clostack"
     :license {:name "MIT License"}
